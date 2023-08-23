1edcc7f2260c26a0d50d8482848f648b1dd29808
par jin sung <parkgutime@gmail.com>
2023-08-23 18:50:12 +0900

    [add] login page test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { Login, Logout } from "./components";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
+  // 전역변수 설정
+  const [login, setLogin] = useState(true);
+  console.log(login);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => setLogin(!login)}>login button</button>
       </header>
+      <Routes>
+        <Route path="/" element={login === true ? <Login /> : <Logout />} />
+      </Routes>
     </div>
   );
 }
